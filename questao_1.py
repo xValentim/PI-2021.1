f656b511ce853b425194407ce916dc456800cb04
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2021-04-23 19:16:56 -0300

    Modificações da questão 1.

--- questao_1.py
+++ questao_1.py
@@ -1,5 +1,8 @@
 '''Valida data
-Faça uma função que recebe três números representando, nessa ordem, um dia, um mês e um ano e devolve True se a data for válida ou False, caso contrário. Na validação, você deve considerar a quantidade de meses e de dias no mês. Lembre-se de que alguns meses têm 30 enquanto outros possuem 31 dias. Também é importante levar em conta os casos de fevereiro e anos bissextos!
+Faça uma função que recebe três números representando, nessa ordem, um dia, um mês e um ano e 
+devolve True se a data for válida ou False, caso contrário. Na validação, você deve considerar a 
+quantidade de meses e de dias no mês. Lembre-se de que alguns meses têm 30 enquanto outros possuem 31 dias. 
+Também é importante levar em conta os casos de fevereiro e anos bissextos!
 
 Anos bissextos
 Existem algumas regras para definir se um ano é bissexto (ou seja, possui o dia 29 de fevereiro):
@@ -10,11 +13,31 @@ Se o ano for múltiplo de 4 e de 100, mas não de 400, ele não é bissexto;
 Caso contrário, ele é bissexto.
 Um exemplo de data válida seria a chamada: valida_data(1, 1, 2021)
 
-Enquanto exemplos de datas inválidas seriam: valida_data(30, 2, 2020), valida_data(31, 4, 2020) ou valida_data(32, 13, 2020).
+Enquanto exemplos de datas inválidas seriam: valida_data(30, 2, 2020), 
+valida_data(31, 4, 2020) ou valida_data(32, 13, 2020).
 
 Você pode assumir que não serão testados valores negativos.
 
 O nome da sua função deve ser valida_data'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def eh_bissexto(a):
     if a % 4 == 0 and not(a % 100 == 0):
